5bcca2820d53c2d1c76f8c9c5f47ff17333ffd75
David Bierce <david@bierce.org>
2025-05-28 15:54:06 -0500

    feat: initial commit

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-max-line-length': [2, 'always', 120],
+        'footer-max-line-length': [2, 'always', 120],
+        'header-max-length': [2, 'always', 72],
+    },
+    ignores: [
+        (commit) => commit.startsWith("chore(deps):")
+    ],
+};
